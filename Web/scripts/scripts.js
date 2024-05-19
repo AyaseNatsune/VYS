7aca7e623350e7f2d481c6089559ff4bd5a69f08
Ayase <144401914+AyaseNatsune@users.noreply.github.com>
2024-05-19 18:50:40 +0900

    Add files via upload

---
+++ Web/scripts/scripts.js
@@ -0,0 +1,8 @@
+console.log('javascript loaded');
+$(function() {
+    $('html,body').animate({ scrollTop: 0 }, '1');
+});
+$(window).on('load',function() {
+  $("#splash").delay(2000).fadeOut('slow',function(){
+});
+});
