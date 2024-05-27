cec88e9e66180dfece8f8583642849da9b956d9a
Ayase <144401914+AyaseNatsune@users.noreply.github.com>
2024-05-27 20:28:56 +0900

    Delete Web directory

--- Web/scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-console.log('javascript loaded');
-$(function() {
-    $('html,body').animate({ scrollTop: 0 }, '1');
-});
-$(window).on('load',function() {
-  $("#splash").delay(2000).fadeOut('slow',function(){
-});
-});
